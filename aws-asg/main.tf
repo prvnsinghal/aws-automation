c4d82316f681b396dcb4a3c1bd368e1571ef306c
prvnsinghal <praveen.kumar100@infosys.com>
2017-04-18 17:54:20 +0530

    Update main.tf

--- aws-asg/main.tf
+++ aws-asg/main.tf
@@ -1,7 +1,7 @@
 # Specify the provider and access details
 provider "aws" {
-  access_key = "<access_key>"
-  secret_key = "<secret_key>"
+  access_key = "XXXXXXXXXXXXXXXXXXXXXXXXX"
+  secret_key = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
   region = "${var.aws_region}"
 }
 
@@ -45,6 +45,22 @@ resource "aws_autoscaling_group" "web-asg" {
   }
 }
 
+data "aws_route53_zone" "selected" {
+  name         = "infy-devops.com."
+}
+
+resource "aws_route53_record" "asgrecord" {
+  zone_id = "${data.aws_route53_zone.selected.zone_id}"
+  name    = "asgrecord.infy-devops.com"
+  type    = "A"
+
+  alias {
+    name                   = "${aws_elb.web-elb.dns_name}"
+    zone_id                = "${aws_elb.web-elb.zone_id}"
+    evaluate_target_health = true
+  }
+}
+
 resource "aws_launch_configuration" "web-lc" {
   name          = "terraform-example-lc"
   image_id      = "${lookup(var.aws_amis, var.aws_region)}"
@@ -54,6 +70,29 @@ resource "aws_launch_configuration" "web-lc" {
   security_groups = ["${aws_security_group.default.id}"]
   user_data       = "${file("userdata.sh")}"
   key_name        = "${var.key_name}"
+}
+
+resource "aws_autoscaling_policy" "bat" {
+  name                   = "foobar3-terraform-test"
+  scaling_adjustment     = 1
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 30
+  autoscaling_group_name = "${aws_autoscaling_group.web-asg.name}"
+}
+
+
+resource "aws_cloudwatch_metric_alarm" "foobar" {
+ alarm_name                = "terraform-test-foobar5"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "1"
+  metric_name               = "CPUUtilization"
+  namespace                 = "AWS/EC2"
+  period                    = "60"
+  statistic                 = "Average"
+  threshold                 = "50"
+
+  alarm_description = "This metric monitor ec2 cpu utilization"
+  alarm_actions     = ["${aws_autoscaling_policy.bat.arn}"]
 }
 
 # Our default security group to access
